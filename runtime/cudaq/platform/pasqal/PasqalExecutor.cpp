063fdfd8314cc437d6a001f3a2bf08d77c0aad76
Kaonan Micadei <k.micadei@gmail.com>
2025-01-31 17:24:24 +0000

    add PasqalExecutor

---
+++ runtime/cudaq/platform/pasqal/PasqalExecutor.cpp
@@ -0,0 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 2022 - 2025 NVIDIA Corporation & Affiliates.                  *
+ * All rights reserved.                                                        *
+ *                                                                             *
+ * This source code and the accompanying materials are made available under    *
+ * the terms of the Apache License 2.0 which accompanies this distribution.    *
+ ******************************************************************************/
+ 
+ #include "PasqalExecutor.h"
+ 
+
+ namespace cudaq {
+
+// TODO: Implement executor.
+details::future PasqalExecutor::execute(std::vector<KernelExecution> &codesToExecute,
+                          bool isObserve) {
+  return {};
+}
+
+ } // namespace cudaq
